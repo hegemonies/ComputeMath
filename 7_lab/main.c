c961d5f212b78118f973d6faad919bc731e05a0b
hegemonies <hegemonies@yandex.ru>
2018-04-06 18:00:22 +0700

    Fix 7_lab

--- 7_lab/main.c
+++ 7_lab/main.c
@@ -1,43 +1,31 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-double Polinom_Newton(double x_find, double *x, double *y, int n)
+double P_Newton(double x_find, double *x, double *y, int n)
 {
-	double res_add = 0.0;
-	double res_mult = 1.0;
-	// double a = 0.0;
-
-	// for (int m = 0; m =< n; m++) { // Pn(x)
-	// 	for (int i = 0; i < m; i++) { // Ai
-	// 		for (int j = 0; j =< i; j++) {
-	// 			res_mult *= x[i] - x[j];
-	// 		}
-	// 		res_add += y[i] / res_mult;
-	// 	}
-	// 	a = res_add * (x_find - x[m]);
-	// }
-
 	double res = 0.0;
-
-	for (int m = 1; m < n - 1; m++) {
-		for (int i = 1; i < m + 1; i++) {
-			for (int j = 1; j < m + 1; j++) {
-				if (i != j) {
-					res_mult *= x[i] - x[j];
+	double res_mult;
+	double res_add;
+	for (int i = 1; i < n - 1; i++) {
+		res_add = 0.0;
+		for (int j = 0; j < i + 1; j++) {
+			res_mult = 1.0;
+			for (int k = 0; k < i + 1; k++) {
+				if (j != k) {
+					res_mult *= x[j] - x[k];
 				}
 			}
-			res_add += y[i] / res_mult;
+			res_add += y[j] / res_mult;
 		}
-		for (int i = 0; i < m; i++) {
-			res = x_find - x[i];
+		res_mult = 1.0;
+		for (int j = 0; j < i; j++) {
+			res_add *= (x_find - x[j]);
 		}
-		res = res_add * res;
-		// printf("check\n");
-	}
-	// printf("\n");
-	
 
-	// return res_add;
+		res += res_add;
+	}
+
+
 	return res + y[0];
 }
 
@@ -50,10 +38,10 @@ int main()
 
 	FILE *out = fopen("out.txt", "w");
 
-	double k = 3.2;
+	double k = 0;
 
-	for (int i = 0; i < 10; i++) {
-		fprintf(out, "%.3lf %.3lf\n", k, Polinom_Newton(k, x, y, n));
+	for (int i = 0; i < 40; i++) {
+		fprintf(out, "%.3lf %.3lf\n", k, P_Newton(k, x, y, n));
 		k += 0.2;
 	}
 
